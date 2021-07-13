484de129d93697a81b7161c751413e32ea86e1a0
João R C Almeida <joaorca@gmail.com>
2021-07-12 23:21:31 -0300

    Finalizando Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,65 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons';
+import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar({githubUser}) {
+  return (
+      <Box>
+        <img src={`https://github.com/${githubUser}.png`}
+             style={{borderRadius: '8px'}} alt={githubUser}/>
+      </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'joaorca';
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho',
+  ];
+
+  return (
+      <>
+        <AlurakutMenu githubUser={githubUser}/>
+        <MainGrid>
+          <div className={'profileArea'} style={{gridArea: 'profileArea'}}>
+            <ProfileSideBar githubUser={githubUser}/>
+          </div>
+          <div className={'welcomeArea'} style={{gridArea: 'welcomeArea'}}>
+            <Box>
+              <h1 className={"title"}>
+                Bem vindo(a)
+              </h1>
+
+              <OrkutNostalgicIconSet />
+            </Box>
+          </div>
+          <div className={'profileRelationsArea'}
+               style={{gridArea: 'profileRelationsArea'}}>
+            <ProfileRelationsBoxWrapper>
+              <h2 className={'smallTitle'}>Favoritos
+                ({pessoasFavoritas.length})</h2>
+              <ul>
+                {pessoasFavoritas.map((item) => {
+                  return (
+                      <li key={item}>
+                        <a href={`/users/${item}`}>
+                          <img src={`https://github.com/${item}.png`}
+                               alt={item}/>
+                          <span>{item}</span>
+                        </a>
+                      </li>
+                  );
+                })}
+              </ul>
+            </ProfileRelationsBoxWrapper>
+          </div>
+        </MainGrid>
+      </>
+  );
 }
